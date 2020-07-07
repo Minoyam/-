a3c6e4089b11e2ff9fbe16dfb1b87cfa5509dbb3
mino <k3000js@naver.com>
2020-07-07 18:34:16 +0900

    16 bindingAdapter 추가

--- app/src/main/java/com/cnm/umbrellaalarm/adapter/BindingAdapter.kt
+++ app/src/main/java/com/cnm/umbrellaalarm/adapter/BindingAdapter.kt
@@ -2,7 +2,13 @@ package com.cnm.umbrellaalarm.adapter
 
 import android.view.inputmethod.EditorInfo
 import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.cnm.umbrellaalarm.R
+import com.cnm.umbrellaalarm.data.model.NaverGeocodeResponse
+import com.cnm.umbrellaalarm.data.model.WeatherResponse
 
 @BindingAdapter("bind:bindOnEditorActionListener")
 fun bindOnEditorActionListener(editText: EditText, click: (() -> Unit)) {
@@ -14,5 +20,38 @@ fun bindOnEditorActionListener(editText: EditText, click: (() -> Unit)) {
         }
         true
     }
+}
+
+@BindingAdapter("bind:bindSetImage")
+fun ImageView.bindSetImage(item: WeatherResponse?) {
+    when (item?.current?.weather?.get(0)?.main) {
+        "Clear" -> {
+            setImageResource(R.drawable.ic_clear)
+        }
+        "Clouds" -> {
+            setImageResource(R.drawable.ic_cloud)
+        }
+        "Snow" -> {
+            setImageResource(R.drawable.ic_snow)
+        }
+        "Rain", "Drizzle", "Thunderstorm" -> {
+            setImageResource(R.drawable.ic_rain)
+        }
+        else -> {
+            setImageResource(R.drawable.ic_fog)
+        }
+    }
+}
+
+@BindingAdapter("bind:bindSetTemp")
+fun TextView.bindSetTemp(item: WeatherResponse?) {
+    text = String.format(resources.getString(R.string.tv_temp), item?.current?.temp)
+}
+@BindingAdapter("bind:bindSetAddressItem")
+fun RecyclerView.bindSetAddressItem(items: List<NaverGeocodeResponse.Addresse>?) {
+    if (adapter is AddressAdapter)
+        items?.let {
+            (adapter as AddressAdapter).setItem(it)
+        }
 }
 
